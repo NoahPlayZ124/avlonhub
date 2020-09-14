6bef416de5a5fb577e0ef5089c464ebcfe7b629d
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2020-09-14 20:35:15 +0200

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,8 +1,8 @@
 print("Loading AvlonHub!")
 if game.PlaceId == 292439477 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/avlonhub/master/phantomforces.lua", true))()
-elseif game.PlaceId == 286090429 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/avlonhub/master/arsenal.lua", true))()
+--elseif game.PlaceId == 286090429 then
+  --loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/avlonhub/master/arsenal.lua", true))()
 else
-  game.Players.LocalPlayer:Kick("We do not support this game!")
+  game.Players.LocalPlayer:Kick("We do not support this game!\n\nSupported Games:\n- Phantom Forces")
 end
