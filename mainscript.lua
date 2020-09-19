c64f14be1fdec1f1650ebba64e14c82265a1f8f7
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2020-09-19 18:28:13 +0200

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,10 +1,6 @@
 print("Loading AvlonHub!")
 if game.PlaceId == 292439477 then
-  if _G.V2_Alpha == true then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/avlonhub/master/PhantomForces-V2.Alpha", true))()
-  else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/avlonhub/master/phantomforces.lua", true))()
-  end
 --elseif game.PlaceId == 286090429 then
   --loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/avlonhub/master/arsenal.lua", true))()
 else
